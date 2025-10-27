079b8469560bb68962b39a8bb261a8ea9792bd43
Sergey Shulga <shul.sergio@gmail.com>
2025-10-27 23:40:16 +0200

    add function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  interface hexToDecFunctionProps {
+    hexData: string;
+  }
+  const [hexinput, setHexInput] = useState<string>("");
+  const [result, setResult] = useState(0);
+
+  function hexToDecFunction({ hexData }: hexToDecFunctionProps) {
+    console.log("Button clicked");
+    return setResult(parseInt(hexData, 16));
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -8,14 +20,30 @@ export default function Home() {
           Welcome
         </h1>
 
-        {/* <div className="total">
+        <div className="total">
           <input
             type="text"
             placeholder="Type something..."
             className="input-field"
+            value={hexinput}
+            onChange={(e) => setHexInput(e.target.value)}
           />
-          <button className="submit-button">Submit</button>
-        </div> */}
+          <button
+            type="submit"
+            onClick={() => hexToDecFunction({ hexData: hexinput })}
+            className="submit-button"
+          >
+            Submit
+          </button>
+        </div>
+        {result && (
+          <p className="result-text">First, result in dec system: {result}</p>
+        )}
+        {result && (
+          <p className="result-text">
+            Second, result in dec system: {result % 64}
+          </p>
+        )}
       </main>
     </div>
   );
