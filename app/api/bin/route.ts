2f78febb928952eb6458b16046c4d6fc3d95ad99
Sergey Shulga <shul.sergio@gmail.com>
2026-01-15 01:52:48 +0200

    fix errors

--- app/api/bin/route.ts
+++ app/api/bin/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { spawn } from "child_process";
 import path from "path";
 import fs from "fs"; //
+export const dynamic = 'force-dynamic';
+
 
 export async function GET() {
   return NextResponse.json({ 
